68bfa7058daffe050c4c82aa441628e07d4ef7a1
sssreeya21-SS <sssreeya21@gmail.com>
2026-04-29 10:43:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 print("STARTED")
-
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, f1_score
 from sklearn.ensemble import RandomForestClassifier
-
 print("IMPORT DONE")
 
 data = pd.read_csv("data/crime.csv")
@@ -12,18 +10,14 @@ print("DATA LOADED")
 
 X = data.drop("target", axis=1)
 y = data["target"]
-
 print("SPLITTING")
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-
-model = RandomForestClassifier()
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+model = RandomForestClassifier(random_state=42)
 model.fit(X_train, y_train)
-
 print("MODEL IS TRAINED")
 
 y_pred = model.predict(X_test)
-
 accuracy = accuracy_score(y_test, y_pred)
 f1 = f1_score(y_test, y_pred, average='weighted')
 
